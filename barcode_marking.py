4afaa1b9da760598975cd88534021d98b92738fc
NandanPaT-eL <nandanpatel1402@gmail.com>
2024-12-27 14:08:12 +0530

    Edited barcode reader

--- barcode_marking.py
+++ barcode_marking.py
@@ -1,25 +1,25 @@
 import cv2
 import numpy as np
 
-data = np.load("calibration_IMX335.npz")
-mtx = data["camera_matrix"]
-dist = data["distortion_coefficients"]
-print("Calibration data loaded successfully.")
+# data = np.load("calibration_IMX335.npz")
+# mtx = data["camera_matrix"]
+# dist = data["distortion_coefficients"]
+# print("Calibration data loaded successfully.")
 
 cap = cv2.VideoCapture(0)
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-map1, map2 = cv2.initUndistortRectifyMap(mtx, dist, None, mtx, (width, height), cv2.CV_32FC1)
+# map1, map2 = cv2.initUndistortRectifyMap(mtx, dist, None, mtx, (width, height), cv2.CV_32FC1)
 
 detector = cv2.barcode_BarcodeDetector()
 
 while True:
     ret, frame = cap.read()
     
-    undistorted_frame = cv2.remap(frame, map1, map2, interpolation=cv2.INTER_LINEAR)
-    gray = cv2.cvtColor(undistorted_frame, cv2.COLOR_BGR2GRAY)
+    # undistorted_frame = cv2.remap(frame, map1, map2, interpolation=cv2.INTER_LINEAR)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
